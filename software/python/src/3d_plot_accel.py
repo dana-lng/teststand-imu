e8b6413f40d250e26da4f980e8e1d2d2f258dbdc
Anh Le Xuan <anh.lexuan05@gmail.com>
2025-10-25 17:02:00 +0200

    updating

---
+++ software/python/src/3d_plot_accel.py
@@ -0,0 +1,85 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+
+
+# Define calibration parameters
+A = np.array([[1.004332, 0.000046, 0.004896],  # 'A^-1' matrix from Magneto
+              [0.000046, 0.969793, 0.009452],
+              [0.004896, 0.009452, 1.022384]])
+# 'Combined bias (b)' vector from Magneto
+b = np.array([0.027031, -0.040204, 0.046558])
+
+# Pfade
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, "../../..", "data")  # ../data relativ zu src/
+CSV = os.path.join(DATA_DIR, "data.csv")
+
+rawData = pd.read_csv(CSV)
+rawData = rawData.to_numpy()
+np.set_printoptions(edgeitems=30, linewidth=1000, formatter={'float': '{: 0.4f}'.format})
+units = 'G\'s'  # units of accelerometer measurements (used for axis labels)
+
+N = len(rawData)
+calibData = np.zeros((N, 3), dtype='float')
+for i in range(N):
+    currMeas = np.array([rawData[i, 0], rawData[i, 1], rawData[i, 2]])
+    calibData[i, :] = A @ (currMeas - b)
+
+"""
+# Plot XY data
+plt.figure()
+plt.plot(rawData[:, 0], rawData[:, 1], 'b*', label='Raw Meas.')
+plt.plot(calibData[:, 0], calibData[:, 1], 'r*', label='Calibrated Meas.')
+plt.title('XY Accelerometer Data')
+plt.xlabel('X [{}]'.format(units))
+plt.ylabel('Y [{}]'.format(units))
+plt.legend()
+plt.grid()
+plt.axis('equal')
+
+# Plot YZ data
+plt.figure()
+plt.plot(rawData[:, 1], rawData[:, 2], 'b*', label='Raw Meas.')
+plt.plot(calibData[:, 1], calibData[:, 2], 'r*', label='Calibrated Meas.')
+plt.title('YZ Accelerometer Data')
+plt.xlabel('Y [{}]'.format(units))
+plt.ylabel('Z [{}]'.format(units))
+plt.legend()
+plt.grid()
+plt.axis('equal')
+
+# Plot XZ data
+plt.figure()
+plt.plot(rawData[:, 0], rawData[:, 2], 'b*', label='Raw Meas.')
+plt.plot(calibData[:, 0], calibData[:, 2], 'r*', label='Calibrated Meas.')
+plt.title('XZ Accelerometer Data')
+plt.xlabel('X [{}]'.format(units))
+plt.ylabel('Z [{}]'.format(units))
+plt.legend()
+plt.grid()
+plt.axis('equal')
+"""
+
+# Plot 3D scatter
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+for i in range(N):
+    xraw = rawData[i, 0]
+    yraw = rawData[i, 1]
+    zraw = rawData[i, 2]
+
+    xcalib = calibData[i, 0]
+    ycalib = calibData[i, 1]
+    zcalib = calibData[i, 2]
+    ax.scatter(xraw, yraw, zraw, color='r', label='Raw')
+    ax.scatter(xcalib, ycalib, zcalib, color='b', label='Calibrated')
+
+ax.set_title('3D Scatter Plot of Accelerometer Data')
+ax.set_xlabel('X [{}]'.format(units))
+ax.set_ylabel('Y [{}]'.format(units))
+ax.set_zlabel('Z [{}]'.format(units))
+ax.legend(['Raw', 'Calibrated'])
+
+plt.show()
